f8e3dd1b253bfbc3834145044267bbdd2a93db19
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-09-10 15:09:13 +0100

    adding sql sechma

---
+++ src/main/java/SqlSchema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE hotels (
+                        id int PRIMARY KEY,
+                        name VARCHAR(255) NOT NULL,
+                        address VARCHAR(255) NOT NULL,
+                        phone VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE rooms (
+                       id INT PRIMARY KEY,
+                       hotel_id INT REFERENCES hotels(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                       room_number VARCHAR(10) NOT NULL,
+                       isAvailable boolean NOT NULL ,
+                       capacity int NOT NULL,
+                       price DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE reservations (
+                              id INT PRIMARY KEY,
+                              room_id INT REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE ,
+                              user_id INT REFERENCES clients(id) ON DELETE CASCADE ON UPDATE CASCADE ,
+                              client VARCHAR(100) NOT NULL,
+                              check_in DATE NOT NULL,
+                              check_out DATE NOT NULL,
+                              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE clients (
+                         id int primary key ,
+                         firstname varchar(255) NOT NULL ,
+                         lastname varchar(255) NOT NULL
+);
+
+ALTER TABLE reservations
+    ADD COLUMN user_id INT REFERENCES clients(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE reservations
+DROP COLUMN client ;
+
