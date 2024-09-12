c6014d51d8ff9d123fd3cc2ef504436eb6602636
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-09-12 10:56:56 +0100

    Creating MEnu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,54 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+        System.out.println(
+                "╔════════════════════════════════════════════════════════════════════════════════════╗\n" +
+                        "║                                                                                    ║\n" +
+                        "║                                 Hotel Reservation System                           ║\n" +
+                        "║                                                                                    ║\n" +
+                        "╚════════════════════════════════════════════════════════════════════════════════════╝");
+        int input;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("\n║\t1: Hotels Reservation \t\t\t║");
+            System.out.println("║\t2: Rooms Managment \t\t\t\t║");
+            System.out.println("║\t3: Reservations Managment \t \t║");
+            System.out.println("║\t4: Exit\t\t\t\t\t\t\t║");
+            System.out.print("\t Enter your choice: ");
+
+            input = sc.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            switch (input) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("!!!! Invalid choice. Please enter a number between 1 and 5. !!!!");
+            }
+
+        } while (input != 6);
+
+        sc.close();
     }
 }
